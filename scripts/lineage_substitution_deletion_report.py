a48d879b61bd8df6cc5f29e30e4b33944dc8324b
Animesh Kumar Singh <animesh.workplace@gmail.com>
2022-05-23 10:51:57 +0530

    updated file - scripts/lineage_substitution_deletion_report.py

---
+++ scripts/lineage_substitution_deletion_report.py
@@ -0,0 +1,38 @@
+import os
+import pandas
+import argparse
+import itertools
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--metadata", help="Enter Nexstrain metadata")
+parser.add_argument("--output", help="Enter output file")
+args = parser.parse_args()
+metadata_url = args.metadata
+output_url = args.output
+
+metadata = pandas.read_csv(metadata_url, delimiter="\t", encoding="utf-8")
+
+new_dict = []
+for i in metadata.index:
+    all_changes = []
+    if isinstance(metadata.iloc[i]["aaSubstitutions"], str):
+        all_changes.append(metadata.iloc[i]["aaSubstitutions"].split(","))
+    if isinstance(metadata.iloc[i]["aaDeletions"], str):
+        all_changes.append(metadata.iloc[i]["aaDeletions"].split(","))
+    all_changes = list(itertools.chain(*all_changes))
+    p = [
+        new_dict.append(
+            {
+                "strain": metadata.iloc[i]["strain"],
+                "lineage": metadata.iloc[i]["lineage"],
+                "mutation/deletion": j,
+                "call": metadata.iloc[i]["scorpio_call"],
+                "date": metadata.iloc[i]["date"],
+            }
+        )
+        for j in all_changes
+    ]
+
+pandas.DataFrame.from_dict(data=new_dict, orient="columns").to_csv(
+    output_url, sep="\t", header=True, index=False
+)
